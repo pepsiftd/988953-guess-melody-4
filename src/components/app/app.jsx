945671269e2ff102ff04c6884854e5c468be20ae
pepsiftd <pepsiftd@gmail.com>
2020-06-25 13:57:43 +0300

    Добавляет Router и исправляет jsx разметку атрибута style

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
 import WelcomeScreen from '@/components/welcome-screen/welcome-screen';
+import QuestionArtist from '@/components/question-artist/question-artist';
+import QuestionGenre from '@/components/question-genre/question-genre';
 
 const welcomeButtonClickHandler = () => {};
 
 const App = ({mistakesCount}) => {
-  return <WelcomeScreen
-    mistakesCount={mistakesCount}
-    welcomeButtonClickHandler={welcomeButtonClickHandler}
-  />;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <WelcomeScreen
+            mistakesCount={mistakesCount}
+            welcomeButtonClickHandler={welcomeButtonClickHandler}
+          />
+        </Route>
+        <Route exact path="/question-artist">
+          <QuestionArtist />
+        </Route>
+        <Route exact path="/question-genre">
+          <QuestionGenre />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 App.propTypes = {
